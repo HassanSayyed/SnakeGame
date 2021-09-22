322a97b36ba937977b85a50757ab822978ec5391
HassanSayyed <hasanalsayyed651998@gmail.com>
2021-09-23 00:39:58 +0300

    Snake Project

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,7 +5,7 @@ public class GameFrame extends JFrame{
 	GameFrame(){
 		GamePanel panel = new GamePanel();
 		this.add(panel);
-		this.setTitle("Snake Game");
+		this.setTitle("Snake Game ");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
